cf3cb3c3475f8140d48fe996bdaf17d02f4e0f01
Todd Willey <todd@lapex.(none)>
2009-03-27 00:49:54 -0400

    Fix scoping of login for account: only initialize once, no matter how broke rails is.

--- lib/user_system_has_accounts_sessions_controller.rb
+++ lib/user_system_has_accounts_sessions_controller.rb
@@ -1,9 +1,12 @@
 module UserSystemHasAccountsSessionsController
   private
   def self.included kls
-    kls.send :alias_method_chain, :login_scope, :accounts
+    unless kls.private_instance_methods.include?('login_scope_without_accounts')
+      kls.send :alias_method_chain, :login_scope, :accounts
+    end
   end
 
+  private
   def login_scope_with_accounts
     login_scope_without_accounts.for_account(current_account)
   end
